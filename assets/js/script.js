559a4d6241e7a48fe93bc2ab0e1e7d410c586022
Pavel Belodurin <belod.pav@gmail.com>
2017-01-11 15:05:47 +0300

    some functinality changes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@
 var buttons = document.getElementsByTagName("button"),
     fDisplay = document.getElementsByClassName("calc-display-first")[0],
     sDisplay = document.getElementsByClassName("calc-display-second")[0],
-    marker = 0;
+    marker = 0,
+    markerPointF = 0,
+    markerPointS = 0,
+    markerFirstItem = 0,
+    markerDigits = 0,
+    valueF = 0,
+    valueS = 0;
     
 
 // Add symbols to displays
@@ -11,14 +17,33 @@ function addToFirstDisplay(str) {
     fDisplay.innerHTML += str;
 }
 function addToSecondDisplay(str) {
-    fDisplay.innerHTML += str;
+    sDisplay.innerHTML += str;
 }
 // Clear both displays (AC) 
 function firstDisplayClear() {
     fDisplay.innerHTML = '';
+    
 }
 function secondDisplayClear() {
     sDisplay.innerHTML = '';
+}
+
+function addOperation(operator) {
+    valueS = parseFloat(sDisplay.innerHTML);   
+    switch (operator) {
+            case "+":
+                valueF +=valueS;
+                if (markerFirstItem !== 0) {
+                    secondDisplayClear();  
+                    sDisplay.innerHTML = valueF.toString();      
+                }
+                
+                markerFirstItem = 1;
+                
+                console.log(valueF,"  ",valueS);
+                break;
+    }
+    
 }
 // Clear current display
 function ceLastDelite(marker) {
@@ -40,14 +65,33 @@ document.onclick = function(element) {
     if (value === "ac") {
         firstDisplayClear();
         secondDisplayClear();
+        valueF = 0;
+        valueS = 0;
+        markerFirstItem = 0;
+        markerDigits = 0;
         return;
     } 
+
     switch(value) {
         case "ce": 
             ceLastDelite(marker);
             break;
+        case "+": 
+            if (markerDigits === 1) {
+                addToFirstDisplay(sDisplay.innerHTML+"+");
+                addOperation("+");
+                markerDigits = 0;
+                
+            }
+            
+            break;   
         default :
-            addToFirstDisplay(value);
+            if (markerDigits === 0) {
+                secondDisplayClear();
+            }
+            //addToFirstDisplay(sDisplay.innerHTML);
+            addToSecondDisplay(value);
+            markerDigits = 1;
             break;
     }
 };
